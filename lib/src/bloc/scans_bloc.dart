6cf9a8dc89df2bc3f48c10b62e5aeb1e144ed5d6
Eduard Cala <eduardcala@gmail.com>
2020-09-27 12:33:44 -0500

    161. ScanBloc - Usando Streams

---
+++ lib/src/bloc/scans_bloc.dart
@@ -0,0 +1,33 @@
+
+
+
+import 'dart:async';
+
+import 'package:qrreaderapp/src/providers/db_provider.dart';
+
+class ScansBloc {
+  
+  static final ScansBloc _singleton = new ScansBloc._internal();
+
+  factory ScansBloc() {
+    return _singleton;
+  }
+
+  ScansBloc._internal() {
+    // Obtener Scans de la Base de datos
+  }
+
+
+  final _scansController = StreamController<List<ScanModel>>.broadcast();
+
+  Stream<List<ScanModel>> get scansStream => _scansController.stream;
+
+  dispose() {
+    _scansController?.close();
+  }
+
+
+}
+
+
+
