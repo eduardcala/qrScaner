e9b0b3d2c480a046b9bb279660f6af8f3fd45433
Eduard Cala <eduardcala@gmail.com>
2020-09-29 22:40:10 -0500

    169. Crear un mapa en Flutter

--- lib/src/pages/mapa_page.dart
+++ lib/src/pages/mapa_page.dart
@@ -1,28 +1,56 @@
 import 'package:flutter/material.dart';
+
+import 'package:flutter_map/flutter_map.dart';
+
 import 'package:qrreaderapp/src/models/scan_model.dart';
 
 
-
 class MapaPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
 
-    final ScanModel scan = ModalRoute.of(context).settings.arguments;
+    final ScanModel scan = ModalRoute.of(context).settings.arguments; 
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Coordenadas QR'),
-        actions: [
+        actions: <Widget>[
           IconButton(
             icon: Icon(Icons.my_location),
-            onPressed: () {}
+            onPressed: (){},
           )
         ],
       ),
-      body: Center(
-        child: Text(scan.valor),
-      ),
+      body: _crearFlutterMap(scan)
     );
   }
+
+  Widget _crearFlutterMap( ScanModel scan ) {
+
+    return FlutterMap(
+      options: MapOptions(
+        center: scan.getLatLng(),
+        zoom: 10
+      ),
+      layers: [
+        _crearMapa(),
+      ],
+    );
+
+  }
+
+  _crearMapa() {
+
+    return TileLayerOptions(
+      urlTemplate: 'https://api.mapbox.com/v4/'
+      '{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}',
+      additionalOptions: {
+        'accessToken': 'pk.eyJ1Ijoia2xlcml0aCIsImEiOiJjanY2MjF4NGIwMG9nM3lvMnN3ZDM1dWE5In0.0SfmUpbW6UFj7ZnRdRyNAw',
+        'id': 'mapbox.streets'
+      }
+    );
+
+
+  }
+
 }
